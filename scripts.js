d7f36a596482ad80e8fd74fffb8fbc25dc5b09aa
meyersw <meyersw@otc.edu>
2024-07-19 16:57:57 -0500

    project complete

---
+++ scripts.js
@@ -0,0 +1,2 @@
+// document.write("<footer class='bg-dark text-light'>Thank you!</footer>");
+document.getElementById("theFoother").innerHTML = document.write("<footer class='bg-dark text-light'>Thank you!</footer>");
